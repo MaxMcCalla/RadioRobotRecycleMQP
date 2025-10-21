f59b192c8043003d31eb88087bf7b443be2dbf82
= <maxwellmccalla@gmail.com>
2025-10-21 12:51:00 -0400

    Setting up a test program, needs power supply and wiring done

--- Motion/R3MQP Motion/src/main.cpp
+++ Motion/R3MQP Motion/src/main.cpp
@@ -1,13 +1,20 @@
 #include <Arduino.h>
+#include <AccelStepper.h>
 
 // put function declarations here:
 int myFunction(int, int);
+AccelStepper Step1(AccelStepper::FULL4WIRE,2,4,5,18);
+
 
 void setup() {
+  Step1.setMaxSpeed(200.0);
+  Step1.setAcceleration(100.0);
+  Step1.moveTo(24);
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
