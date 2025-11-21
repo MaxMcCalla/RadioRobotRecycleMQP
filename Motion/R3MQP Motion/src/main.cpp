c7e4006a46a3683fbe5fba42167dbf00f470e4d7
= <maxwellmccalla@gmail.com>
2025-11-21 11:26:51 -0500

    Created functions for reseting and moving motors and started on state machine logic

--- Motion/R3MQP Motion/src/main.cpp
+++ Motion/R3MQP Motion/src/main.cpp
@@ -1,14 +1,47 @@
 #include <Arduino.h>
 #include <AccelStepper.h>
 
+const int STATE_RESET_ENCODERS = 0;
+const int STATE_MOVE_ARM = 1;
+
+int buttonPins[4] = {1,2,3,4};
+
+int ticksToDegrees[4] = {1,1,1,1};
+
 // put function declarations here:
 AccelStepper Step1(AccelStepper::FULL2WIRE,12,14);
 AccelStepper Step2(AccelStepper::FULL2WIRE, 27,26);
 AccelStepper Step3(AccelStepper::FULL2WIRE, 25,33);
 AccelStepper Step4(AccelStepper::FULL2WIRE, 32,35);
 
+AccelStepper Motors[4] = {Step1,Step2,Step3,Step4};
+
+int state;
+
+bool getLimitSwitch(int switchID){
+  return digitalRead(buttonPins[switchID]);
+}
+
+void resetMotors(){
+  for(int i = 0; i < sizeof(Motors); i++){
+    Motors[i].setCurrentPosition(0);
+  }
+}
+
+void moveMotor(int motorID, double position){
+  Motors[motorID].moveTo(position);
+  Motors[motorID].run();
+}
+
+void moveAllMotors(double motorPositions[]){
+  for(int i = 0; i < sizeof(motorPositions); i++){
+    moveMotor(i,motorPositions[i]);
+  }
+}
+
 
 void setup() {
+  state = STATE_RESET_ENCODERS;
   Step1.setMaxSpeed(200.0);
   Step1.setAcceleration(100.0);
   Step2.setMaxSpeed(200.0);
@@ -17,23 +50,23 @@ void setup() {
   Step3.setAcceleration(100.0);
   Step4.setMaxSpeed(200.0);
   Step4.setAcceleration(100.0);
-  Step1.move(1000);
-  Step1.run();
-
-  delay(1000);
+//  delay(1000);
   // put your setup code here, to run once:
   
 }
 
 void loop() {
-  Step2.moveTo(1000);
-  Step2.run();
-  Step1.moveTo(-100);
-  Step1.run();
-  Step3.moveTo(300);
-  Step3.run();
-  Step4.moveTo(500);
-  Step4.run();
+  if(state == STATE_RESET_ENCODERS){
+
+    Step1.setCurrentPosition(0);
+    Step2.setCurrentPosition(0);
+    Step3.setCurrentPosition(0);
+    Step4.setCurrentPosition(0);
+
+  } 
+  if(state == STATE_MOVE_ARM){
+
+  }
   //delay(100);
   // put your main code here, to run repeatedly:
 
